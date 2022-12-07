d18473c3ff406a97e6f10a15a302a0a9779af17f
ashvv1 <ashaviv27@hotmail.com>
2022-12-07 22:15:35 +0200

    styling update - fade header

--- src/App.js
+++ src/App.js
@@ -41,101 +41,101 @@ function App() {
   const logoCaption = useRef(null);
 
 
-const icons = [reactIcon, nodeIcon, mongoIcon, jsIcon, dockerIcon, firebaseIcon, awsIcon, htmlIcon, cssIcon];
-  
-const cacheImages = async (srcArray) => {
-  const promises = await srcArray.map((src) => {
-    return new Promise(function (resolve, reject) {
-      const img = new Image();
-      img.src = src;
-      img.onload = resolve();
-      img.onerror = reject();
+  const icons = [reactIcon, nodeIcon, mongoIcon, jsIcon, dockerIcon, firebaseIcon, awsIcon, htmlIcon, cssIcon];
+
+  const cacheImages = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
     });
-  });
 
-  await Promise.all(promises);
+    await Promise.all(promises);
 
-  setIsLoading(false)
-}
+    setIsLoading(false)
+  }
 
-useEffect(() => {
-  const imgs = [
-    loading,
-    logo,
-    realAdam,
-    reactIcon,
-    nodeIcon,
-    mongoIcon,
-    jsIcon,
-    dockerIcon,
-    firebaseIcon,
-    awsIcon,
-    htmlIcon,
-    cssIcon,
-    safesend,
-    cheats,
-    crypto,
-    galquiz,
-    linkedinIcon,
-    resumeIcon,
-    emailIcon,
-    vsSnip,
-    vsSnipTwo,
-    resumePdf,
-    githubIcon    
-  ]
+  useEffect(() => {
+    const imgs = [
+      loading,
+      logo,
+      realAdam,
+      reactIcon,
+      nodeIcon,
+      mongoIcon,
+      jsIcon,
+      dockerIcon,
+      firebaseIcon,
+      awsIcon,
+      htmlIcon,
+      cssIcon,
+      safesend,
+      cheats,
+      crypto,
+      galquiz,
+      linkedinIcon,
+      resumeIcon,
+      emailIcon,
+      vsSnip,
+      vsSnipTwo,
+      resumePdf,
+      githubIcon
+    ]
 
-  cacheImages(imgs);
-}, [])
+    cacheImages(imgs);
+  }, [])
 
   const projects = [
     {
-        name:"TextSOD",
-        description:"Send and receive messages using a 5 Digit Code that is known only by you and your intended respondents",
-        tech: ["Node", "React"],
-        images: [safesend],
-        link: "http://textsod.com"
+      name: "TextSOD",
+      description: "Send and receive messages using a 5 Digit Code that is known only by you and your intended respondents",
+      tech: ["Node", "React"],
+      images: [safesend],
+      link: "http://textsod.com"
     },
     {
-        name: "My Quiz Gal",
-        description: "Learn any language that you want through practice and by testing yourself, for free!",
-        tech: ["Node", "React", "Router", "CSS", "HTML"],
-        images: [galquiz],
-        link: "https://www.myquizgal.com/"
+      name: "My Quiz Gal",
+      description: "Learn any language that you want through practice and by testing yourself, for free!",
+      tech: ["Node", "React", "Router", "CSS", "HTML"],
+      images: [galquiz],
+      link: "https://www.myquizgal.com/"
     },
     {
-        name: "Cheat Delete",
-        description: "Cheat food calorie calculator and workout planner.",
-        tech: ["Node", "JSX"],
-        images: [cheats],
-        link: "https://ashvv1.github.io/cheatdelete"
+      name: "Cheat Delete",
+      description: "Cheat food calorie calculator and workout planner.",
+      tech: ["Node", "JSX"],
+      images: [cheats],
+      link: "https://ashvv1.github.io/cheatdelete"
     },
     {
-        name: "Crypto Calc",
-        description: "Calculate potential gains or losses on trades of XRP/BTC/ETH",
-        tech: ["Node", "React", "JSX"],
-        images: [crypto],
-        link: "https://ashvv1.github.io/cryptocalc/"
+      name: "Crypto Calc",
+      description: "Calculate potential gains or losses on trades of XRP/BTC/ETH",
+      tech: ["Node", "React", "JSX"],
+      images: [crypto],
+      link: "https://ashvv1.github.io/cryptocalc/"
     }
 
-];
+  ];
 
   const switchLogo = () => {
     setClickCount((prevCount) => {
       return prevCount + 1
     });
-    if(clickCount > 3){
+    if (clickCount > 3) {
       setCapMessage("Ouch! How many times are you gonna click me?")
     }
-    if(icon === logo){
+    if (icon === logo) {
       setIcon(realAdam);
-    }else{
+    } else {
       setIcon(logo);
     }
-    if(!captionActive){
+    if (!captionActive) {
       logoCaption.current.style.display = 'flex';
       setCaptionActive(true);
-    }else{
+    } else {
       logoCaption.current.style.display = 'none';
       setCaptionActive(false);
     }
@@ -149,27 +149,27 @@ useEffect(() => {
     switch (ref) {
       case sectionOne:
         appWrapper.current.scrollTo({
-                top: 0,
-                behavior: "smooth"
-           });
+          top: 0,
+          behavior: "smooth"
+        });
         break;
       case sectionTwo:
         appWrapper.current.scrollTo({
-          top: appHeight *.3333 + 1,
+          top: appHeight * .3333 + 1,
           behavior: "smooth"
-     });
+        });
         break;
       case sectionThree:
         appWrapper.current.scrollTo({
-          top: appHeight *.6666 + 1,
+          top: appHeight * .6666 + 1,
           behavior: "smooth"
-     });
+        });
         break;
-      default: 
+      default:
         break;
     }
-    }
-   
+  }
+
 
   const checkIfInView = (element) => {
     const elementBounds = element.current.getBoundingClientRect();
@@ -183,12 +183,12 @@ useEffect(() => {
   const handleScroll = () => {
     const appHeight = appWrapper.current.getBoundingClientRect().height * 2;
     const topElement = sectionOne.current.getBoundingClientRect().top;
-    setProgress((Math.abs(topElement * 1.111237)/ appHeight) * 100 )
-    if(checkIfInView(sectionOne)){
+    setProgress((Math.abs(topElement * 1.111237) / appHeight) * 100)
+    if (checkIfInView(sectionOne)) {
       setActive('about');
-    }else if(checkIfInView(sectionTwo)){
+    } else if (checkIfInView(sectionTwo)) {
       setActive('work');
-    }else if(checkIfInView(sectionThree)){
+    } else if (checkIfInView(sectionThree)) {
       setActive('contact');
     }
   }
@@ -210,8 +210,8 @@ useEffect(() => {
       <div className='header-container' ref={sectionOne}>
         <header>
           <div className='logo-container'>
-          <img onClick = {() => switchLogo()} src={icon} alt="homelogo" className='logo'></img>
-          <div className ='logo-caption' ref={logoCaption} ><span>{capMessage}</span></div>
+            <img onClick={() => switchLogo()} src={icon} alt="homelogo" className='logo'></img>
+            <div className='logo-caption' ref={logoCaption} ><span>{capMessage}</span></div>
           </div>
           <nav >
             <ul className='nav-list'>
@@ -220,9 +220,9 @@ useEffect(() => {
               <li className={active === 'contact' ? 'active' : ""} onClick={() => goToSection(sectionThree)}>Contact</li>
             </ul>
             <div className='progress-ball-container'>
-            <div className='progress-ball' style={{ left: `${progress}%` }}></div>
+              <div className='progress-ball' style={{ left: `${progress}%` }}></div>
             </div>
-            
+
           </nav>
         </header>
       </div>
@@ -230,10 +230,19 @@ useEffect(() => {
       <div className='body'>
         <div id="about" className='section column'>
           <div className="headerText" >
-           <h4>Hi, my name is</h4>
-           <h2>Adam Haviv</h2>
-           <h3>I am a FullStack Developer</h3>
-           <p>I specialize in building websites and apps using React and NoSQL databases. Currently, I am working as an intern, building a Learning Management System from the ground up as part of a small team.</p>
+            <div id='upOne'>
+            <h4>Hi, my name is</h4>
+            <h2>Adam Haviv</h2>
+            </div>
+            <div id='upTwo'>
+            <h3>I am a FullStack Developer</h3>
+            </div>
+            <div id='upThree'>
+            <p>I specialize in building websites and apps using React and NoSQL databases. 
+              Currently, I am working as an intern, building a Learning Management System from the ground up as part of a small team.</p>
+            </div>
+            
+            
           </div>
 
           <img className='vs-snip' src={vsSnip} alt="vscode snippet" hidden={active !== 'about'}></img>
@@ -243,39 +252,39 @@ useEffect(() => {
           <div className="icons-container">
             {icons.map((icon, i) => (
               <img key={`icon${i}`} src={icon} alt='stack icon'></img>
-          ))}
-            </div>
-            <div className="Project-list">
-                {projects.map((project, i) => (
-                    <div key={project.name} className="Project-list-item">
-                      <div className="project-list-desc">
-                        <h3>{project.name}</h3>
-                        <h4>{project.description}</h4>
-                        <br></br>
-                        </div>
-                        <div className="tech-tiles">{project.tech.map((item, i) => (
-                            <div key={item+i}className="tech-tile">{item}</div>
-                        ))}
-                        </div>
-                        <div className="image-gallery">
-                            {project.images.map((image,i) => (
-                                <a key = {`${project.name}image${i}`} href={project.link}>
-                                    <img src={image} alt='gallery' ></img>
-                                </a>
-                                
-                            ))}
-                        </div>
-                    </div>
+            ))}
+          </div>
+          <div className="Project-list">
+            {projects.map((project, i) => (
+              <div key={project.name} className="Project-list-item">
+                <div className="project-list-desc">
+                  <h3>{project.name}</h3>
+                  <h4>{project.description}</h4>
+                  <br></br>
+                </div>
+                <div className="tech-tiles">{project.tech.map((item, i) => (
+                  <div key={item + i} className="tech-tile">{item}</div>
                 ))}
-            </div>
-            <img className='vs-snip' src={vsSnipTwo} alt="vscode snippet" hidden={active !== 'work'}></img>
+                </div>
+                <div className="image-gallery">
+                  {project.images.map((image, i) => (
+                    <a key={`${project.name}image${i}`} href={project.link}>
+                      <img src={image} alt='gallery' ></img>
+                    </a>
+
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <img className='vs-snip' src={vsSnipTwo} alt="vscode snippet" hidden={active !== 'work'}></img>
         </ div>
 
         <div id='contact' ref={sectionThree} className='section'>
-          <a href='https://www.linkedin.com/in/adam-haviv-84bb17225'><img src={linkedinIcon} alt='linkedin icon'/></a>
+          <a href='https://www.linkedin.com/in/adam-haviv-84bb17225'><img src={linkedinIcon} alt='linkedin icon' /></a>
           <a href={resumePdf} download='adamhavivresume.pdf'><img src={resumeIcon} alt='resume icon'></img></a>
           <div id='email-icon-container'>
-          <img onClick={() => openEmail()} id='email-icon' src={emailIcon} alt='email icon'></img>
+            <img onClick={() => openEmail()} id='email-icon' src={emailIcon} alt='email icon'></img>
           </div>
           <a href={'https://github.com/ashvv1/'} ><img src={githubIcon} alt='github icon'></img></a>
         </div>
